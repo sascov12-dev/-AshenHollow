b9ac5b0c312b23452555fb2d4c8b6aae463c4b95
sascov12-dev <sascov12@gmail.com>
2026-08-31 16:45:39 +0300

    Add files via upload

--- Sources/GameView.swift
+++ Sources/GameView.swift
@@ -3,14 +3,17 @@ import SpriteKit
 
 struct GameView: View {
     private var scene: SKScene {
-        let scene = GameScene(size: CGSize(width: 390, height: 844))
+        let scene = GameScene(size: CGSize(width: 844, height: 390))
         scene.scaleMode = .resizeFill
         return scene
     }
 
     var body: some View {
-        SpriteView(scene: scene)
-            .ignoresSafeArea()
-            .background(Color.black)
+        SpriteView(
+            scene: scene,
+            options: [.ignoresSiblingOrder]
+        )
+        .ignoresSafeArea()
+        .background(Color.black)
     }
 }
